3199b67066dfbee31d34cc3aaa9e9514c5443b8e
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2021-03-31 00:12:01 -0500

    created user authentication flow
    will style later

--- models/authenticate.js
+++ models/authenticate.js
@@ -10,8 +10,11 @@ const redirectUri = 'http://localhost/oauth/redirect'
 
 module.exports = class Authenticate {
     constructor(windows, session) {
+        //set local window and session from main
         this.windows = windows
         this.session = session
+
+        //oath params
         this.params = {
             client_id: config.clientId,
             client_secret: config.clientSecret,
@@ -19,36 +22,73 @@ module.exports = class Authenticate {
             redirect_uri: redirectUri,
             scope: 'the scopes'
         }        
+
+        //set userdata to null then fill it as we get it
+        this.userData = {
+            access_token: null,
+            expires_in: null,
+            refresh_token: null,
+            scope: null,
+            token_type: null,
+            id: null,
+            username: null,
+            avatar: null,
+            discriminator: null,           
+        }
     }
+
+    //check if the user is already logged in
     isAuthenticated = () => {
-        return false
+        const access_token = store.get('access_token')
+        const token_type = store.get('token_type')
+
+        return new Promise((resolve, reject) => {
+            if(!access_token || !token_type) { reject(false) }
+
+            return this.getUserdata(token_type, access_token)
+            .then(res => this.localizeInfo(res.data))
+            .then(() => { resolve(true) } )
+            .catch(() => { reject(false) })
+        })
     }
+
+    //open the auth window
     openAuth = () => {
         this.windows.modal.loadURL(config.authUrl)
         this.windows.modal.show()
         this.waitForRedirect()        
     }
 
+    //wait for a redirection from the auth window
     waitForRedirect = () => {
 
+        //filter our uri
         const filter = {
             urls: [redirectUri + '*']
         }
 
         this.session.defaultSession.webRequest.onBeforeRequest(filter, (details, callback) => {
-
             const urlObj = url.parse(details.url, true)
             
+            //credentials are good
             if (urlObj.query.code) {
                 const accessCode = urlObj.query.code
-                this.params.push({ code: accessCode })
+                this.params.code = accessCode
                 this.windows.modal.close()
                 //loging you in message
-                
+
+                //get the oauth info and store it for later
+                this.authenicateUser()
+                .then(res => this.localizeInfo(res.data))
+                .then(res => this.getUserdata(this.userData.token_type, this.userData.access_token))
+                .then(res => this.localizeInfo(res.data))
+                .then(() => this.storeUserInfo())
                 
                 this.windows.main.loadFile(path.join(app.getAppPath(), 'app', 'settings.html'))
 
             }
+
+            //display an error if there is one; or user cancels
             if(urlObj.query.error) {
                 const errorCode = urlObj.query.error
                 const errorDesc = urlObj.query.error_description
@@ -61,10 +101,9 @@ module.exports = class Authenticate {
         })
     }
 
-    authenicateUser = (accessCode) => {
-
-
-        axios({
+    //API to discord auth to check users credentials
+    authenicateUser = () => {
+        return axios({
             method: 'POST',
             url: 'https://discord.com/api/oauth2/token',
             data: new URLSearchParams(this.params),
@@ -74,26 +113,30 @@ module.exports = class Authenticate {
         })        
     }
 
+    //get the users data after they've logged in
+    getUserdata = (tokenType, token) => {
+        return axios({
+            method: 'get',
+            url: 'https://discord.com/api/users/@me',
+            headers: {
+                authorization: `${tokenType} ${token}`,
+            }
+        })
+    }
 
-    
+    //store the info we want from discords API response
+    localizeInfo = (payload) => {
+        return new Promise ((resolve, reject) => {
+            Object.keys(payload).filter(key => key in this.userData).forEach(key => this.userData[key] = payload[key])
+            resolve(payload)
+        })
+    }
 
-    
-
-                // .then(res => {
-                //     //store token info
-                //     return res.data
-                // })
-                // .then(info => axios({
-                //     method: 'get',
-                //     url: 'https://discord.com/api/users/@me',
-                //     headers: {
-                //         authorization: `${info.token_type} ${info.access_token}`,
-                //     }                
-                // }))
-                // .then(res => {
-                //     console.log(res.data)
-                // })                 
-            // }
-        
-    // }
+    //save the users tokens for later logins
+    storeUserInfo = () => {
+        const userData = Object.fromEntries(Object.entries(this.userData).filter(([_, v]) => v != null))
+        for(const [key, value] of Object.entries(userData)) {
+            store.set(key, value)
+        }
+    }
 }
