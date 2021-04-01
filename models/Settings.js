628639e17688e197a58b8d20b5e6443108133778
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2021-04-01 17:45:37 -0500

    initial settings

---
+++ models/Settings.js
@@ -0,0 +1,20 @@
+const { rejects } = require('assert')
+const Store = require('electron-store')
+const { resolve } = require('url')
+const store = new Store({ name : 'config.main' })
+
+module.exports = class Settings {
+    settingsSet = () => {
+        const saveDirectory = store.get('saveDirectory')
+        const database = store.get('database') 
+
+        console.log(saveDirectory)
+        
+        if(!saveDirectory || !database) { return false }
+        else { return true }
+    }
+    saveSettings = (payload) => {
+        store.set('saveDirectory', payload.saveDirectory)
+        store.set('database', payload.database)  
+    }
+}
--- models/ipsend.js
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer  } = require('electron')
-
-module.exports = class IPSend {
-    static sendToMain = (name, payload) => {
-        const sendData = {case : name, ...payload}
-        ipcRenderer.send('ipSend', sendData)
-    }
-}
