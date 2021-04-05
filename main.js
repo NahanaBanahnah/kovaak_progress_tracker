64cc816eec92d206e5b2fd3ebceef9939238091f
Nahana <37905054+NahanaBanahnah@users.noreply.github.com>
2021-04-04 20:02:58 -0500

    styling
    Styled elements

--- main.js
+++ main.js
@@ -1,84 +1,165 @@
-'use strict'
+const {
+	app,
+	BrowserWindow,
+	ipcMain,
+	session,
+	dialog,
+	Menu,
+	Tray,
+} = require('electron')
 
-const { app, BrowserWindow, ipcMain, session, dialog   } = require('electron')
+// try {
+// 	require('electron-reloader')(module)
+// } catch (_) {}
 
 const path = require('path')
-
+const fs = require('fs')
 const Authenticate = require(path.join(__dirname, 'models', 'Authenticate.js'))
 const Settings = require(path.join(__dirname, 'models', 'Settings.js'))
 const Home = require(path.join(__dirname, 'models', 'Home.js'))
 
-let windows = {}
+const windows = {}
+let tray = null
 
 const authenticate = new Authenticate(windows, session)
 const settings = new Settings()
-const home = new Home(windows)
 
-
+// create the main window and the auth modal
 const createWindows = () => {
-    let mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        }
-    })
-    windows.main = mainWindow
+	const mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		center: true,
+		frame: false,
+		show: false,
+		backgroundColor: '#FFF',
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+		},
+	})
+	windows.main = mainWindow
 
-    let modal = new BrowserWindow({
-        modal: true,
-        show: false,     
-        parent: mainWindow,
-        width: 800,
-        height: 800,   
-    })
-    windows.modal = modal
+	const modal = new BrowserWindow({
+		modal: true,
+		show: false,
+		parent: mainWindow,
+		width: 800,
+		height: 800,
+	})
+	windows.modal = modal
+	windows.modal.setMenuBarVisibility(false)
+
+	windows.main.loadFile(path.join(__dirname, 'app', 'main.html'))
 }
-
-const openAuth = () => {
-    authenticate.openAuth()  
-}
-
-ipcMain.on('toMain', (e, payload) => {
-    switch(payload.case) {
-        default :
-
-        break
-        case 'openAuth' :
-            openAuth()
-        break
-        case 'fileBrowser' :
-            dialog.showOpenDialog({ 
-                properties: ['openDirectory'],
-                title: 'Browse For Your Kovaak Instalation Directory'                
-            }).then(file => {
-                if(!file.canceled) {
-                    windows.main.send('fromMain', {case : 'filePicked', file : file.filePaths })
-                }
-            })
-        break
-        case 'saveSettings' :
-            settings.saveSettings(payload)
-        break
-    }
-})
 
 app.whenReady().then(() => {
-    createWindows()
+	//create the windows
+	createWindows()
 
-    authenticate.isAuthenticated()
-    .then(isAuthenticated => {
-        if(!settings.settingsSet()) {
-            windows.main.loadFile(path.join(__dirname, 'app', 'Settings', 'settings.html'))
-        } else {
-            windows.main.loadFile(path.join(__dirname, 'app', 'Home', 'home.html'))
-            home.initWatch()
-        }        
-        windows.main.show()
-    }).catch(e => {
-        windows.main.loadFile(path.join(__dirname, 'app', 'Login', 'login.html'))
-        windows.main.show()
-    }) 
+	//create the tray
+	tray = new Tray(path.join(__dirname, 'app', 'icons', 'tray', 'icon.png'))
+
+	const contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Show App',
+			click() {
+				windows.main.show()
+			},
+		},
+		{
+			label: 'Quit',
+			click() {
+				windows.main.destroy()
+				app.quit()
+			},
+		},
+	])
+	tray.setToolTip('Kovaak Progress Tracker')
+	tray.setContextMenu(contextMenu)
+
+	//load main window with loading bar once the DOM is ready
+	windows.main.once('ready-to-show', () => {
+		windows.main.show()
+
+		authenticate
+			.isAuthenticated()
+			.then(() => {
+				if (!settings.settingsSet()) {
+					sendContent('home', 'settings', true)
+				} else {
+					const home = new Home(windows)
+					sendContent('home', 'home')
+					home.initWatch()
+				}
+			})
+			.catch(() => sendContent('login'))
+	})
+
+	//check if the user is authnticated
+	//if yes check if they have settings
+	//if no open the auth
+
+	windows.main.on('close', e => {
+		e.preventDefault()
+		windows.main.hide()
+	})
 })
 
+ipcMain.on('toMain', (e, payload) => {
+	switch (payload.case) {
+		default:
+			break
+		case 'openAuth':
+			authenticate.openAuth()
+			break
+		case 'fileBrowser':
+			dialog
+				.showOpenDialog({
+					properties: ['openDirectory'],
+					title: 'Browse For Your Kovaak Instalation Directory',
+				})
+				.then(file => {
+					if (!file.canceled) {
+						const dir = path.join(
+							file.filePaths[0],
+							'FPSAimTrainer',
+							'stats'
+						)
+						let error = false
+						const stat = fs.stat(dir, (error, stats) => {
+							if (error) {
+								error = true
+							}
+							windows.main.send('folderSelect', {
+								case: 'filePicked',
+								file: file.filePaths,
+								error: error,
+							})
+						})
+					}
+				})
+			break
+		case 'saveSettings':
+			settings.saveSettings(payload)
+			break
+	}
+})
 
+// Helper Function
+const sendContent = (file, section = false, initial = false) => {
+	fs.readFile(
+		path.join(__dirname, 'app', 'html', `${file}.html`),
+		'utf8',
+		(err, html) => {
+			const payload = {
+				...authenticate.getUserInfo(),
+				...settings.getSettings(),
+				html: html,
+				section: section,
+				initial: initial,
+				version: app.getVersion(),
+			}
+			windows.main.send('loadContent', payload)
+		}
+	)
+}
